d6b5296fb19031ed49ead3bed92cfc96115d8923
HamzehSaleh <sa.hamzeh@hotmail.com>
2022-10-24 19:36:46 +0300

    config the navbar and main screens

---
+++ Client/electrotech/src/pages/home/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LayoutApp from "../../components/Layout";
+
+const Home = () => {
+  return <LayoutApp>Home</LayoutApp>;
+};
+
+export default Home;
---
+++ Client/electrotech/src/pages/products/Products.js
@@ -0,0 +1,8 @@
+import React, { useState } from "react";
+import LayoutApp from "../../components/Layout";
+
+const Products = () => {
+  return <LayoutApp>Products</LayoutApp>;
+};
+
+export default Products;
